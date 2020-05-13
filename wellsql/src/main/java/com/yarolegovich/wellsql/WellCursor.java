27176abbcd9b8e588ac3af3a6042bf807388024f
Nick Bradbury <nick.bradbury@gmail.com>
2020-05-13 11:56:00 -0400

    Enlarge the cursor window size to avoid SQLiteBlobTooBigExceptions

--- wellsql/src/main/java/com/yarolegovich/wellsql/WellCursor.java
+++ wellsql/src/main/java/com/yarolegovich/wellsql/WellCursor.java
@@ -1,8 +1,11 @@
 package com.yarolegovich.wellsql;
 
 import android.database.Cursor;
+import android.database.CursorWindow;
 import android.database.CursorWrapper;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.support.annotation.Nullable;
 
 import com.yarolegovich.wellsql.mapper.SelectMapper;
@@ -14,9 +17,15 @@ public class WellCursor<T> extends CursorWrapper {
 
     private SQLiteDatabase mDb;
     private SelectMapper<T> mMapper;
+    private static final long CURSOR_WINDOW_SIZE = 1024L * 1024L * 5L;
 
     WellCursor(SQLiteDatabase db, SelectMapper<T> mapper, Cursor cursor) {
         super(cursor);
+
+        // Enlarge the cursor window size to avoid SQLiteBlobTooBigExceptions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ((SQLiteCursor) cursor).setWindow(new CursorWindow(null, CURSOR_WINDOW_SIZE));
+        }
 
         mDb = db;
         mMapper = mapper;
